40d1c2e021a80029533bfcbb69421ed9311c6bac
Albin Ekblom <albinekblom@gmail.com>
2018-02-14 20:28:52 +0100

    🌹 More tests

--- index.test.js
+++ index.test.js
@@ -1,5 +1,48 @@
 const parseEnv = require('./index.js')
 
+test('Parses BASIC=basic', () => {
+  expect(parseEnv('BASIC=basic')).toEqual(['BASIC', 'basic'])
+})
+test('Parses UNDEFINED_EXPAND=$TOTALLY_UNDEFINED_ENV_KEY', () => {
+  expect(parseEnv('UNDEFINED_EXPAND=$TOTALLY_UNDEFINED_ENV_KEY')).toEqual([
+    'UNDEFINED_EXPAND',
+    '$TOTALLY_UNDEFINED_ENV_KEY',
+  ])
+})
+test(`Parses SINGLE_QUOTES='single_quotes'`, () => {
+  expect(parseEnv(`SINGLE_QUOTES='single_quotes'`)).toEqual([
+    'SINGLE_QUOTES',
+    'single_quotes',
+  ])
+})
+test(`Parses DOUBLE_QUOTES="double_quotes"`, () => {
+  expect(parseEnv(`DOUBLE_QUOTES="double_quotes"`)).toEqual([
+    'DOUBLE_QUOTES',
+    'double_quotes',
+  ])
+})
+test(`Parses EQUAL_SIGNS=equals==`, () => {
+  expect(parseEnv(`EQUAL_SIGNS=equals==`)).toEqual(['EQUAL_SIGNS', 'equals=='])
+})
+test(`Parses RETAIN_INNER_QUOTES={"foo": "bar"}`, () => {
+  expect(parseEnv(`RETAIN_INNER_QUOTES={"foo": "bar"}`)).toEqual([
+    'RETAIN_INNER_QUOTES',
+    `{"foo": "bar"}`,
+  ])
+})
+test(`Parses RETAIN_INNER_QUOTES_AS_STRING='{"foo": "bar"}'`, () => {
+  expect(parseEnv(`RETAIN_INNER_QUOTES_AS_STRING='{"foo": "bar"}'`)).toEqual([
+    'RETAIN_INNER_QUOTES_AS_STRING',
+    `{"foo": "bar"}`,
+  ])
+})
+test(`Parses INCLUDE_SPACE=some spaced out string`, () => {
+  expect(parseEnv(`INCLUDE_SPACE=some spaced out string`)).toEqual([
+    'INCLUDE_SPACE',
+    `some spaced out string`,
+  ])
+})
+
 test('Parses NODE_ENV=production', () => {
   expect(parseEnv('NODE_ENV=production')).toEqual(['NODE_ENV', 'production'])
 })
